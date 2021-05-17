e445dd56f0388a0a245d30c817005e17c6a636a4
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-05-17 15:39:22 +0100

    Projeto base

---
+++ backend/src/test/java/com/pelosi/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelosi.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
