3b808ba9d79def05b9568caac7a51c403909726e
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-05-19 11:35:34 +0100

    Home navigattion

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,22 +9,22 @@ const Dashboard = () => {
         <div className="">
             <>
                 <div>
-                    <NavBar />
+                    <NavBar/>
                     <div className="row px-3">
                         <div className="col-sm-6">
                             <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-                            <BarChart />
+                            <BarChart/>
                         </div>
                         <div className="col-sm-6">
                             <h5 className="text-center text-secondary">Todas as vendas</h5>
-                            <DonutChart />
+                            <DonutChart/>
                         </div>
                     </div>
                     <div className="py-3">
                         <h2 className="text-primary">Todas as vendas</h2>
                     </div>
-                    <DataTable />
-                    <Footer />
+                    <DataTable/>
+                    <Footer/>
                 </div>
             </>
         </div>
